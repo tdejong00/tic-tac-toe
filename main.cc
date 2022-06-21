cd0f7232b0e00d1e876087c211b46e285319f705
tdejong <tdejong0212@gmail.com>
2022-06-21 21:33:30 +0200

    Improve command line arguments

--- main.cc
+++ main.cc
@@ -1,43 +1,65 @@
 #include "./include/tictactoe.h"
 #include <cstring>
 
+// Prints list of available command line arguments.
+void print_help() {
+    std::cout << APPNAME << " " << VERSION << std::endl;
+    std::cout << "Play a game of Tic-Tac-Toe against an AI." << std::endl;
+    std::cout << std::endl;
+    std::cout << "Usage: ./tic-tac-toe [OPTIONS]..." << std::endl;
+    std::cout << std::endl;
+    std::cout << "  --help" << std::endl;
+    std::cout << "      Show help for all options" << std::endl;
+    std::cout << "  --version" << std::endl;
+    std::cout << "      Show version number of this application" << std::endl;
+    std::cout << "  --method=<r|m> [default: m]" << std::endl;
+    std::cout << "      Set the method of the AI to either 'r' (AI makes random decisions) or 'm' (AI uses the minimax-algorithm)" << std::endl;
+    std::cout << "  --player=<x|o> [default: o]" << std::endl;
+    std::cout << "      Set the mark of the player to either 'x' (crosses) or 'o' (noughts). The AI will automatically have the other mark." << std::endl;
+    std::cout << std::endl;
+    std::cout << "Exit status:" << std::endl;
+    std::cout << " 0 if OK" << std::endl;
+    std::cout << " 1 if invalid argument or too many arguments" << std::endl;
+}
+
 int main(int argc, char *argv[]) {
-    bool is_random;
-    if (argc == 2) {
-        if (std::strcmp(argv[1], "--help") == 0) {
-            std::cout << APPNAME << " " << VERSION << std::endl;
-            std::cout << "Play a game of Tic-Tac-Toe against an AI." << std::endl;
-            std::cout << std::endl;
-            std::cout << "Usage: ./tic-tac-toe [AI METHOD]..." << std::endl;
-            std::cout << std::endl;
-            std::cout << "  -r, --random      AI makes random decisions" << std::endl;
-            std::cout << "  -m, --minimax     AI uses minimax-algorithm for its decisions" << std::endl;
-            std::cout << std::endl;
-            std::cout << "By default, the AI uses the minimax-algorithm." << std::endl;
-            std::cout << std::endl;
-            std::cout << "Exit status:" << std::endl;
-            std::cout << " 0 if OK" << std::endl;
-            std::cout << " 1 if invalid argument or too many arguments" << std::endl;
-            return 0;
-        }
-        else if (std::strcmp(argv[1], "--random") == 0 || std::strcmp(argv[1], "-r") == 0) {
-            is_random = true;
-        }
-        else if (std::strcmp(argv[1], "--minimax") == 0 || std::strcmp(argv[1], "-m") == 0) {
-            is_random = false;
-        }
-        else {
-            std::cout << "tic-tac-toe: invalid argument '" << argv[1] << "'" << std::endl;
-            return 1;
-        }
-    }
-    else if (argc > 2) {
-        std::cout << "tic-tac-toe: too many arguments were given" << std::endl;
+    bool is_random = false;
+    bool is_crosses = false;
+    if (argc > 4) {
+        std::cout << "tic-tac-toe: too many arguments were given, use --help for a list of accepted arguments" << std::endl;
         return 1;
     }
+    else if (argc > 1) {
+        for (int i = 1; i < argc; i++) {
+            if (std::strcmp(argv[i], "--help") == 0) {
+                print_help();
+                return 0;
+            }
+            if (std::strcmp(argv[i], "--version") == 0) {
+                std::cout << APPNAME << " " << VERSION << std::endl;
+                return 0;
+            }
+            else if (std::strcmp(argv[i], "--method=r") == 0) {
+                is_random = true;
+            }
+            else if (std::strcmp(argv[i], "--method=m") == 0) {
+                is_random = false;
+            }
+            else if (std::strcmp(argv[i], "--player=x") == 0) {
+                is_crosses = true;
+            }
+            else if (std::strcmp(argv[i], "--player=o") == 0) {
+                is_crosses = false;
+            }
+            else {
+                std::cout << "tic-tac-toe: invalid argument '" << argv[1] << "', use --help for a list of accepted arguments" << std::endl;
+                return 1;
+            }
+        }
+    }
 
-    tictactoe* ttt = new tictactoe;
-    ttt->play_game(is_random);
+    tictactoe* ttt = new tictactoe();
+    ttt->play_game(is_random, is_crosses);
     delete ttt;
 
     return 0;
